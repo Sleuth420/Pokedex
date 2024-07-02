ecc0a71ddea70a6f0f730271536299905a865933
Sleuth420 <167057432+Sleuth420@users.noreply.github.com>
2024-07-02 16:32:25 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,49 +2,49 @@ const CACHE_NAME = 'pokedex-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  // Add other static assets here
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png',
+  '/manifest.json',
+  // Add other assets to cache
 ];
 
-// Install a service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          fetchedResponse => {
+            if (!fetchedResponse || fetchedResponse.status !== 200 || fetchedResponse.type !== 'basic') {
+              return fetchedResponse;
+            }
+            const responseToCache = fetchedResponse.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseToCache));
+            return fetchedResponse;
+          }
+        );
       })
   );
 });
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('pokeapi.co')) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return fetch(event.request).then(response => {
-          cache.put(event.request.url, response.clone());
-          return response;
-        }).catch(() => caches.match(event.request));
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          return response || fetch(event.request);
-        })
-    );
-  }
-});
-
-// Update a service worker
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
